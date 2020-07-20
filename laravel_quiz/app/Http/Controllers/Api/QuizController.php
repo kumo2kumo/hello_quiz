19f8fe9a9cae5e3cf606032ae4a597011cba4e4e
kumo2kumo <amgokuraku@gmail.com>
2020-07-21 00:59:43 +0900

    6-6 クイズ表示

--- laravel_quiz/app/Http/Controllers/Api/QuizController.php
+++ laravel_quiz/app/Http/Controllers/Api/QuizController.php
@@ -8,21 +8,21 @@ use Illuminate\Http\Request;
 
 class QuizController extends Controller
 {
-    public function index(Request $request)
-    {
-        $category = $request->input('categories');
-        if ($category) {
-            $category = explode(',', $category);
-        } else {
-            return [];
-        }
+	public function index(Request $request)
+	{
+		$category = $request->input('categories');
+		if ($category) {
+			$category = explode(',', $category);
+		} else {
+			return [];
+		}
 
-        $quiz = Quiz::with(['answer', 'category'])
-            ->wherein('quizzes.categories_id', $category)
-            ->inRandomOrder()
-            ->limit(10)
-            ->get();
+		$quiz = Quiz::with(['answer', 'category'])
+			->wherein('quizzes.categories_id', $category)
+			->inRandomOrder()
+			->limit(10)
+			->get();
 
-        return $quiz;
-    }
+		return $quiz;
+	}
 }
